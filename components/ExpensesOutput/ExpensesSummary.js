e9c7b0c804fb1ec0e83396f89b5c159c22de8d02
Adrian Jurisic <adrianjurisic1601@gmail.com>
2025-02-07 16:31:39 +0100

    Key Components

---
+++ components/ExpensesOutput/ExpensesSummary.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+
+function ExpensesSummary(){
+    return (
+        <View>
+            <Text>Period</Text>
+            <Text>Sum</Text>
+        </View>
+    );
+}
+
+export default ExpensesSummary;
